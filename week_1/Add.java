5a07568d54aa145544e0ef589126b919fbe7bf97
Vaishnavi Tekale <141334633+vaishnavitekle@users.noreply.github.com>
2025-11-04 16:31:40 +0530

    Add Add.java to calculate the sum of two numbers

---
+++ week_1/Add.java
@@ -0,0 +1,8 @@
+class Add {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
