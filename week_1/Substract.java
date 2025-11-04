ea500aaaefd339a6474ed2e3c742687422c69c91
Vaishnavi Tekale <141334633+vaishnavitekle@users.noreply.github.com>
2025-11-04 16:35:15 +0530

    Add Substract.java for basic subtraction example

---
+++ week_1/Substract.java
@@ -0,0 +1,8 @@
+class Sub {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a - b;
+        System.out.println("The substraction of " + a + " and " + b + " is: " + sum);
+    }
+}
