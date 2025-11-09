255d3b70441289c15d7f88d59fbbd719cb174e2e
Vaishnavi Tekale <141334633+vaishnavitekle@users.noreply.github.com>
2025-11-09 18:55:35 +0530

    Create Arraysequal.java

---
+++ week_4/Arraysequal.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class arrayseq {
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,50};
+        int[] arr2={10,20,30,40,50};
+        System.out.println("Arrays Equals: " + Arrays.equals(arr,arr2));
+        arr.equals(arr2);
+    }
+}
